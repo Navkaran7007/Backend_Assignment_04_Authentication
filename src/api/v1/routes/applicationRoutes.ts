1530902966c4eb4342711fafefa8284e75d9b31f
Navkaran7007 <nsidhu10@rrc.ca>
2025-10-30 22:23:20 -0500

    Defined basic Routes in applicationRoutes.ts

---
+++ src/api/v1/routes/applicationRoutes.ts
@@ -0,0 +1,12 @@
+import express from "express";
+const router = express.Router();
+
+router.post("/", );
+
+router.get("/", );
+
+router.put("/:id/review", );
+
+router.put("/:id/approve", );
+
+export default router;
--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,13 @@
 import express, { Express } from "express";
+import morgan from "morgan"
+import applicationRoutes from "./api/v1/routes/applicationRoutes"
 
 // Initialize Express application
 const app: Express = express();
 
+// Use Morgan for HTTP request logging
+app.use(morgan("combined"));
+app.use("/api/v1/loans", applicationRoutes)
 // Define a route
 app.get("/", (req, res) => {
     res.send("Hello, World!");
