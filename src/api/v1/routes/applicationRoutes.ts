81d3f01e83f34c1ce8740fce299c0607e975ff9a
Navkaran7007 <nsidhu10@rrc.ca>
2025-11-04 18:57:39 -0600

    Updated Routes

--- src/api/v1/routes/applicationRoutes.ts
+++ src/api/v1/routes/applicationRoutes.ts
@@ -1,16 +1,29 @@
 import express from "express";
 import { approveloan, createloan, getloan, reviewloan } 
                                 from "../controllers/applicationControllers";
-
+import authenticate from "../middleware/authenticate";
+import isAuthorized from "../middleware/authorize";
                                 
 const router = express.Router();
 
-router.post("/", createloan);
+router.post("/", 
+    authenticate,
+    isAuthorized({ hasRole: ["user"] }), 
+    createloan);
 
-router.get("/", getloan);
+router.get("/", 
+    authenticate,  
+    isAuthorized({ hasRole: ["officer", "manager"] }), 
+    getloan);
 
-router.put("/:id/review", reviewloan);
+router.put("/:id/review",
+     authenticate,
+    isAuthorized({ hasRole: ["officer"], allowSameUser: true }), 
+    reviewloan);
 
-router.put("/:id/approve", approveloan);
+router.put("/:id/approve",
+     authenticate,
+    isAuthorized({ hasRole: ["manager"], allowSameUser: true }), 
+    approveloan);
 
 export default router;
